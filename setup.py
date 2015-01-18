ea48d1d912ee8af4514163c74efece625d9321d6
Jakub Kadlčík <frostyx@email.cz>
2015-01-18 21:06:19 +0100

    Add basic setup.py with author, info and dependencies

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='lazy-snake',
+	version='0.1',
+	description='A web based python profiler',
+	url='https://github.com/FrostyX/lazy-snake',
+
+	author='FrostyX',
+	author_email='frostyx@email.cz',
+
+	install_requires=['flask'],
+)
