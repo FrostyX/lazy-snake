50741072de0267e7483f605f86a562fd8150d99c
Jakub Kadlčík <frostyx@email.cz>
2015-01-19 02:02:43 +0100

    Get static files working on openshift

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,10 @@
 import os
-here = os.path.dirname(os.path.abspath(__file__))
-
 from flask import Flask
-app = Flask("lazy-snake", template_folder=here+"/views")
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+app = Flask("lazy-snake",
+	template_folder=here+"/views",
+	static_folder=here+"/../static"
+)
 app.secret_key = "jklpuzo"
