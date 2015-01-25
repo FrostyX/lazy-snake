a4491bed1a912ee69f34b6a19bb4dcb8d0a585f5
Jakub Kadlčík <frostyx@email.cz>
2015-01-25 17:14:13 +0100

    Let client be configurable by URL in config

--- server/app/config.py
+++ server/app/config.py
@@ -1,5 +1,5 @@
 import os
 
-
-UPLOAD_DIR = os.environ.get("OPENSHIFT_DATA_DIR", "storage/")
+parent = os.path.dirname(os.path.dirname(__file__))
+UPLOAD_DIR = os.environ.get("OPENSHIFT_DATA_DIR", parent + "/storage/")
 RESULTS_DIR = UPLOAD_DIR + "results"
